6ab48422a9f7f6f99af119ce6c2c70253313c12f
Abraar <94352769+Abraar007@users.noreply.github.com>
2023-12-01 15:00:39 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to my webserver-Abr";
+		return "welcome to my webserver @Abraar";
 	}
 
 	public static void main(String[] args) {
